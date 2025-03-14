a04b96ae0ad48e41e6f2de77eee8187993355d80
EirikNilsson <eirik.haugennilsson@elev.ga.ntig.se>
2025-03-14 11:10:58 +0100

    Backup before adding SvelteKit

--- js/clicker.js
+++ js/clicker.js
@@ -2,18 +2,30 @@ let stimClicks = 0;
 let dvdBought = false;
 let dvdMoving = false;
 let luckyClickBought = false;
+let addOneBought = false;
 let numbers = [1, 10, 1, 1, 1, 1, 1, 1, 1, 1];
-
+let numbers2 = [2, 20, 2, 2, 2, 2, 2, 2, 2, 2];
+let numbers3 = [2, 20, 2, 2, 2, 2, 2, 2];
 
 function increaseStimClicks() {
-    if (!luckyClickBought) {
+    if (!luckyClickBought && !addOneBought) {
         stimClicks++;
-        document.getElementById("stimClicksCount").textContent = stimClicks;
-
+    } else if (!addOneBought && luckyClickBought){
+        stimClicks += numbers[Math.floor(Math.random() * numbers.length)]; 
+    } else {
+        stimClicks += numbers2[Math.floor(Math.random() * numbers.length)]; 
     }
+    
+    document.getElementById("stimClicksCount").textContent = stimClicks;
 
     if (stimClicks >= 10 && !dvdBought) {
         document.getElementById("rewardImage").classList.remove("hidden");
+    }
+    if (stimClicks >= 60 && !luckyClickBought) {
+        document.getElementById("rewardImage2").classList.remove("hidden");
+    }
+    if (stimClicks >= 110 && !addOneBought) {
+        document.getElementById("rewardImage3").classList.remove("hidden");
     }
 }
 
@@ -30,9 +42,19 @@ function buyDvD() {
 
 function buyLuckyClick() {
     if (stimClicks >= 100 && !luckyClickBought) {
-        stimClicks += number[Math.floor(Math.random() * number.length)]
+        stimClicks -= 100;
         document.getElementById("stimClicksCount").textContent = stimClicks;
-        
+        document.getElementById("rewardImage2").classList.add("hidden");
+        luckyClickBought = true;
+    }
+}
+
+function buyAddClick() {
+    if (stimClicks >= 150 && !addOneBought) {
+        stimClicks -= 150;
+        document.getElementById("stimClicksCount").textContent = stimClicks;
+        document.getElementById("rewardImage3").classList.add("hidden");
+        addOneBought = true;
     }
 }
 
@@ -52,20 +74,20 @@ function startDVDAnimation() {
 
         if (x <= 0 || x >= screenW) {
             dx = -dx;
-            stimClicks++;};
+            stimClicks++;
+        }
+        
         if (y <= 0 || y >= screenH) {
             dy = -dy;
             stimClicks++;
         }
 
-
+        document.getElementById("stimClicksCount").textContent = stimClicks;
         dvd.style.left = x + "px";
         dvd.style.top = y + "px";
 
         requestAnimationFrame(moveDVD);
-
     }
 
     moveDVD();
 }
-document.getElementById("stimClicksCount").textContent = stimClicks;
